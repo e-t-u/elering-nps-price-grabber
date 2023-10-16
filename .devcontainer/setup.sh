895a5bf2178abd5fbb6efc31dc93139ef6afe333
e-t-u <esa@turtiainen.eu>
2023-10-16 01:46:15 +0000

    Added devcontainer Rust default config files, not tested yet

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,26 @@
+## update and install some things we should probably have
+apt-get update
+apt-get install -y \
+  curl \
+  git \
+  gnupg2 \
+  jq \
+  sudo \
+  fish \
+  jed \
+  build-essential \
+  openssl
+
+## Install rustup and common components
+curl https://sh.rustup.rs -sSf | sh -s -- -y 
+rustup install nightly
+rustup component add rustfmt
+rustup component add rustfmt --toolchain nightly
+rustup component add clippy 
+rustup component add clippy --toolchain nightly
+
+cargo install cargo-expand
+cargo install cargo-edit
+
+## setup and install oh-my-zsh
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
